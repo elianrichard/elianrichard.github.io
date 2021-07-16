92e7763ec6069d69697c3f4cdb4a053ba58619b1
Elian Richard <elianrichards@gmail.com>
2021-07-16 23:03:12 +0700

    Added My Portofolio Website

---
+++ js/projects.js
@@ -0,0 +1,27 @@
+var projectsLists = [
+  {
+    title: 'Ikatan Mahasiswa Elektro FTUI Web Design',
+    heading: 'IME FTUI',
+    image: './assets/screenshot.jpg',
+    site: 'https://ime.eng.ui.ac.id',
+    detail: 'IME FTUI is an Executive Council of Electrical Engineering Students in Universitas Indonesia. This project goal is to achieve <span class="bold light">new, fresh, and user-friendly design</span> to provide useful  information for the students of Electrical Engineering. Aside from information about the organization, they also upload an article monthly, summaries of lecture, and a collection of lecture book to be downloaded by the students. The highlight of this project is to provide a <span class="bold light">great user experience</span> while maintaining the <span class="bold light">simplicity</span> of the website.',
+    subImage: [
+      './assets/screenshot.jpg',
+      './assets/image.jpg',
+      './assets/screenshot.jpg',
+    ],
+  },
+  {
+    title: 'Kamuka Parwata FTUI Web Design',
+    heading: 'KAPA FTUI',
+    image: './assets/image.jpg',
+    site: 'https://kapaftui.id',
+    detail: 'Kamuka Parwata is an organization in Engineering Faculty of Universitas Indonesia which focuses in outer sport adventure. The goal in this project is to provide <span class="bold light">real adventure experience</span> through photography taken from various adventure done by the organization. The organization consist of five division, there are rock climbing, diving, mountaineering, water sports, and caving. The highlight of this website is to show <span class="bold light">the excitement and thrilling experience</span> in outdoor adventure while maintaining the <span class="bold light">simplicity and proffesionalism</span> work shown by the organization.',
+    subImage: [
+      './assets/screenshot.jpg',
+      './assets/image.jpg',
+      './assets/screenshot.jpg',
+    ],
+  },
+]
+var projectsCount = projectsLists.length;
